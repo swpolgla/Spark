07548ee6f698cfcb7d283c76cd339eb074e69a5a
swpolgla <swpolgla@ncsu.edu>
2020-06-26 01:39:26 -0400

    Cleaned up cmake file. Moved sources to new file.

---
+++ sourcelist.cmake
@@ -0,0 +1,9 @@
+set(sources ${sources}
+	"src/minimal.cpp"
+	"src/SparkCalc.cpp"
+	"src/SparkUI.cpp"
+	"src/operation-tree/OperationTree.cpp"
+	"src/operation-tree/nodes/OperationNode.cpp"
+	"src/operation-tree/nodes/MathNode.cpp"
+	"src/operation-tree/nodes/ValueNode.cpp"
+)
